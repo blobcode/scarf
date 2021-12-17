4a68b5297112aa5332a14a205892e55fd9e0f90b
blobcode <lhelme123@gmail.com>
2021-12-17 11:33:47 -0500

    commenting

--- src/pool.rs
+++ src/pool.rs
@@ -1,1 +1,8 @@
 // deals with server pooling / checking if they are active
+// put on a seperate thread
+// process
+// -set polling interval
+// -create list of servers
+// -poll
+// -update list
+// -somehow change main serverlist
